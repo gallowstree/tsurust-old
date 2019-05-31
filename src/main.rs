d639cc4a3b7ec46cb7814339aed2c198a51e8aee
Alejandro Alvarado <alejalvarado@paypal.com>
2019-05-31 15:29:59 -0600

    [master] Parse tile data

--- src/main.rs
+++ src/main.rs
@@ -2,34 +2,14 @@ use std::fs::File;
 use std::io::BufReader;
 use std::io::BufRead;
 use itertools::Itertools;
+use rand::thread_rng;
+use rand::seq::SliceRandom;
+
 
 
 fn main() {
-    let file = File::open("tiles.txt").expect("Can't open tiles.txt");
-    let reader = BufReader::new(file);
-    let tiles = reader
-        .lines()
-        .map(|line_result| line_result.expect("failed to read tile data"))
-        .map(|tile_text| parse_tile(tile_text))
-        .for_each(|tile| println!("{:?}", tile));
-}
+    let deck = Deck::from_file("tiles.txt").expect("Unable to create deck from tiles.txt");
 
-fn parse_tile(tile_text: String) -> Tile {
-    let tile_text = tile_text.replace(" ", "");
-    let digits = tile_text
-        .chars()
-        .map(|char| char.to_digit(10).unwrap());
-
-    let mut paths :[u8;8] = [0;8];
-
-    for (from, to) in digits.tuples() {
-        paths[from as usize] = to as u8;
-        paths[to as usize] = from as u8;
-    }
-
-    let rotation = Rotation::_0;
-
-    Tile {paths, rotation}
 }
 
 #[derive(Debug)]
@@ -41,7 +21,60 @@ enum Rotation {
 }
 
 #[derive(Debug)]
-struct Tile {
-    paths: [u8; 8],
-    rotation: Rotation
+enum Tile {
+    DragonTile,
+    PathTile {
+        paths: [u8; 8],
+        rotation: Rotation
+    }
+}
+
+#[derive(Debug)]
+struct Deck {
+    tiles: Vec<Tile>
+}
+
+impl Deck {
+    pub fn from_file(filename: &str) -> Result<Deck,String>{
+        let file = File::open(filename).or(Err("Can't open file"))?;
+        let reader = BufReader::new(file);
+
+        let tiles: Result<Vec<Tile>, _> = reader
+            .lines()
+            .take_while(|line_result| line_result.is_ok())
+            .map(|tile_text| Deck::parse_tile(tile_text.unwrap()))
+            .inspect(|tile| println!("{:?}", tile))
+            .collect();
+
+        let mut tiles = tiles?;
+
+        let mut rng = thread_rng();
+        tiles.shuffle(&mut rng);
+        tiles.push(Tile::DragonTile);
+
+        Ok(Deck {tiles})
+    }
+
+    pub fn draw_tile(mut self) -> Option<Tile> {
+        self.tiles.pop()
+    }
+
+    fn parse_tile(tile_text: String) -> Result<Tile, String> {
+        let tile_text = tile_text.replace(" ", "");
+        let digits: Result<Vec<u32>, _> = tile_text
+            .chars()
+            .map(|char| char.to_digit(10).ok_or("Tile data must be numeric"))
+            .collect();
+
+        let mut paths :[u8;8] = [0;8];
+
+        for (from, to) in digits?.into_iter().tuples() {
+            paths[from as usize] = to as u8;
+            paths[to as usize] = from as u8;
+        }
+
+        let rotation = Rotation::_0;
+
+        Ok(Tile::PathTile {paths, rotation})
+    }
 }
